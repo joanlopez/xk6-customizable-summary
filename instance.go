e52af99a05e53e4a79f7458c47f87a037f5bba52
Joan López de la Franca Beltran <joanjan14@gmail.com>
2024-11-13 11:48:54 +0100

    Use 'm.vu.State()' and 'm.vu.InitEnv()' for consistent logging

--- instance.go
+++ instance.go
@@ -1,9 +1,7 @@
 package custosummary
 
 import (
-	"fmt"
 	"regexp"
-	"strings"
 
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modules"
@@ -35,62 +33,49 @@ func (m ModuleInstance) Exports() modules.Exports {
 }
 
 func (m ModuleInstance) includeAllMetrics() {
-	m.debug("All metrics will be included")
+	if m.vu.State() != nil {
+		m.vu.State().Logger.Errorln("'includeAllMetrics' must be called in the init context to take effect")
+		return
+	}
+
+	m.vu.InitEnv().Logger.Debugln("All metrics will be included")
 	// TODO: Implement this.
 }
 
 func (m ModuleInstance) excludeAllMetrics() {
-	m.debug("All metrics will be excluded")
+	if m.vu.State() != nil {
+		m.vu.State().Logger.Errorln("'excludeAllMetrics' must be called in the init context to take effect")
+		return
+	}
+
+	m.vu.InitEnv().Logger.Debugln("All metrics will be excluded")
 	// TODO: Implement this.
 }
 
 func (m ModuleInstance) filterMetric(name string) {
-	m.debug("Metric '" + name + "' will be filtered")
+	if m.vu.State() != nil {
+		m.vu.State().Logger.Errorln("'filterMetric' must be called in the init context to take effect")
+		return
+	}
+
+	m.vu.InitEnv().Logger.Debugln("Metric '" + name + "' will be filtered")
 	// TODO: Implement this.
 }
 
 func (m ModuleInstance) filterMetricByRegexp(re string) {
+	if m.vu.State() != nil {
+		m.vu.State().Logger.Errorln("'filterMetricByRegexp' must be called in the init context to take effect")
+		return
+	}
+
 	_, err := regexp.Compile(re)
 	if err != nil {
-		m.error("Metrics regexp '" + re + "' is invalid: " + err.Error())
+		m.vu.InitEnv().Logger.Errorln("Metrics regexp '" + re + "' is invalid: " + err.Error())
 		// FIXME: Can we avoid the 'GoError' and stack trace here?
 		common.Throw(m.vu.Runtime(), err)
 		return
 	}
 
-	m.debug("Metrics will be filtered by regexp '" + re + "'")
+	m.vu.InitEnv().Logger.Debugln("Metrics will be filtered by regexp '" + re + "'")
 	// TODO: Implement this.
-}
-
-// log is a helper method to log (i.e. `console.log`) messages that serves
-// as a workaround for the lack of a proper logger in the module instance
-// during the init context.
-func (m ModuleInstance) log(str string) {
-	str = strings.ReplaceAll(str, "`", "'") // For safety.
-	_, err := m.vu.Runtime().RunString(fmt.Sprintf("console.log(`%s`)", str))
-	if err != nil {
-		panic(err)
-	}
-}
-
-// error is a helper method to error (i.e. `console.error`) messages that serves
-// as a workaround for the lack of a proper logger in the module instance
-// during the init context.
-func (m ModuleInstance) error(str string) {
-	str = strings.ReplaceAll(str, "`", "'") // For safety.
-	_, err := m.vu.Runtime().RunString(fmt.Sprintf("console.error(`%s`)", str))
-	if err != nil {
-		panic(err)
-	}
-}
-
-// debug is a helper method to debug (i.e. `console.debug`) messages that serves
-// as a workaround for the lack of a proper logger in the module instance
-// during the init context.
-func (m ModuleInstance) debug(str string) {
-	str = strings.ReplaceAll(str, "`", "'") // For safety.
-	_, err := m.vu.Runtime().RunString(fmt.Sprintf("console.debug(`%s`)", str))
-	if err != nil {
-		panic(err)
-	}
 }
